6cd8832292f5e37c251bc792d11cbf679786a6a8
Tatiana Borisova <tanyatik@yandex.ru>
2015-01-26 22:13:05 +0300

    Add missing extract() function to zshrc

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -17,3 +17,25 @@ export PATH="/Users/tanyatik/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/us
 export MANPATH="/usr/local/man:/opt/local/share/man/:$MANPATH"
 
 source $ZSH/oh-my-zsh.sh
+
+extract () {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2) tar xvjf $1   ;;
+      *.tar.gz)  tar xvzf $1   ;;
+      *.tar.xz)  tar xvfJ $1   ;;
+      *.bz2)     bunzip2 $1    ;;
+      *.rar)     unrar x $1    ;;
+      *.gz)      gunzip $1     ;;
+      *.tar)     tar xvf $1    ;;
+      *.tbz2)    tar xvjf $1   ;;
+      *.tgz)     tar xvzf $1   ;;
+      *.zip)     unzip $1      ;;
+      *.Z)       uncompress $1 ;;
+      *.7z)      7z x $1       ;;
+      *)         echo "'$1' cannot be extracted via >extract<" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
