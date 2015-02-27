d5d2628f29c036fdf898769e92bbc87c1ffc689a
User Tsborisova <tsborisova@mr02.search.yandex.net>
2015-02-27 12:59:14 +0300

    Simplify

--- zsh/deploy.sh
+++ zsh/deploy.sh
@@ -1,16 +1,14 @@
-#!/bin/zsh
+#!/usr/bin/env sh
 set -e
 
-if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
     curl -L http://install.ohmyz.sh | sh
 fi
 
-DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd )"
+DIR="$(pwd)"
+echo $DIR
+echo $HOME
 
-cp "${DIR}/zshrc.sh" "${HOME}/.zshrc"
-cp "${DIR}/zprofile.sh" "${HOME}/.zprofile"
+cp "$DIR/zsh/zshrc.sh" "$HOME/.zshrc"
+cp "$DIR/zsh/zprofile.sh" "$HOME/.zprofile"
 
-source $HOME/.zshrc
-source $HOME/.zprofile
-
-echo 1
